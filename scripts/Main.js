27be72fb224838691c825b5be60a5e253aba9b2d
Einar Aglen <einar.aglen99@gmail.com>
2021-02-28 20:19:27 +0100

    Kind of a working version

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,20 +1,114 @@
 window.onload = function(){
     const todoContainer = document.getElementById("todo-container");
+    let displayNumber = 2;
 
-    let item = {
-        value: 0,
-        title: "Wash Dish",
-        info: "Wash the damn dishes retard",
-        status: "" 
+    if(localStorage.getItem("data") === null) {
+        localStorage.setItem("data", JSON.stringify(data));
+    }
+    data = JSON.parse(localStorage.getItem("data"));
+
+    document.addEventListener('click', (e) => {
+        if(e.target && e.target.className == 'delete-button') {
+            //edit the todoitem with value e.target.value
+            data.splice(e.target.id, 1);
+            localStorage.setItem("data", JSON.stringify(data));
+            rendreItems();
+        }
+
+        if(e.target && e.target.className == 'drop-down') {
+            let id = e.target.id;
+            let value = e.target.value;
+            changeStatus(id, value);
+        }
+
+        if(e.target && e.target.className == "display-button") {
+            if(e.target.value == 0) {
+                displayNumber = 1;
+                displayButton(0);
+            } else {
+                displayNumber = 2;
+                displayButton(1);
+            }
+            rendreItems();
+        }
+
+        if(e.target && e.target.id == "form-button") {
+            let title = document.getElementById("title");
+            let info = document.getElementById("info");
+            addToData(title.value, info.value);
+            title.value = "";
+            info.value = "";
+            rendreItems();
+        }
+    });
+
+    const searchInput = document.getElementById("search-input");
+    searchInput.addEventListener("keyup", () => {
+        searchFor(searchInput.value);
+    });
+
+    function searchFor(key) {
+        let result = [];
+        data.forEach((item) => {
+            if(item.title.toLowerCase().indexOf(key) != -1 || item.info.toLowerCase().indexOf(key) != -1) {
+                result.push(item);
+            }
+        });
+
+        rendre(result);
     }
 
-    let todoItem = new TodoItem(item);
 
-    todoContainer.appendChild(todoItem.getItem())
+    displayButton(displayNumber);
 
-    function addTodoItem() {
-        let div = document.createElement("div");
-        div.setAttribute("class", "box");
-        todoContainer.appendChild(div);
+    function displayButton(x) {
+        if(x == 0) {
+            document.getElementById("b1").setAttribute("class", "display-button active");
+            document.getElementById("b2").setAttribute("class", "display-button");
+        } else {
+            displayNumber = 2;
+            document.getElementById("b2").setAttribute("class", "display-button active");
+            document.getElementById("b1").setAttribute("class", "display-button");
+        }
     }
+
+    function changeStatus(id, value) {
+        if(value != -1 && data[id].status != value) {
+            data[id].status = value;
+            rendreItems();
+            localStorage.setItem("data", JSON.stringify(data));
+        }
+    }
+
+    function addToData(title, info) {
+        let item = {
+            title: title,
+            info: info,
+            status: -1
+        }
+
+        data.push(item);
+        localStorage.setItem("data", JSON.stringify(data));
+    }
+
+    // index of item to remove, how many to remove from index
+    //data.splice(1, 1);
+
+    function rendre(data) {
+        while(todoContainer.firstChild) {
+            todoContainer.firstChild.remove();
+        }
+        data.forEach((item, index) => {
+            if(item.status != displayNumber) {
+                let todoItem = new TodoItem(item, index);
+                todoContainer.appendChild(todoItem.getItem());
+            }
+        });
+    }
+
+    function rendreItems() {
+        rendre(data);
+    }
+
+    rendreItems();
 };
--- scripts/TodoItem.js
+++ scripts/TodoItem.js
@@ -1,24 +1,25 @@
 class TodoItem {
-    constructor(item) {
+    constructor(item, value) {
         this.item = item;
+        this.value = value;
     }
 
     getItem() {
         let item = document.createElement("div");
         item.setAttribute("class", "box");
-        item.setAttribute("value", this.item.value);
+        item.setAttribute("value", this.value);
 
         item.appendChild(this.buildTitle());
         item.appendChild(this.buildInfo());
         item.appendChild(this.buildStatus());
-        item.appendChild(this.buildEdit());
+        item.appendChild(this.buildDelete());
 
         return item;
     }
 
     buildTitle() {
         let title = document.createElement("div");
-        title.setAttribute("class", "title");
+        title.setAttribute("class", "title " + "t" + (parseInt(this.item.status) + 1));
         title.innerHTML = this.item.title;
         return title;
     }
@@ -31,22 +32,26 @@ class TodoItem {
     }
 
     buildStatus() {
-        let states = ["Select Status", "Doing", "Done"];
+        let states = ["Status", "In-Progress", "Done"];
         let status = document.createElement("div");
         let dropDown = document.createElement("select");
-        status.setAttribute("class", "status")
-        dropDown.setAttribute("class", "drop-down")
+        status.setAttribute("class", "status");
+        dropDown.setAttribute("class", "drop-down");
+        dropDown.setAttribute("id", this.value);
 
         for(let i = 0; i < states.length; i++) {
             let option = document.createElement("option");
             option.innerHTML = states[i];
             if(i == 0) {
-                option.setAttribute("value", "");
-                option.disabled;
-                option.selected;
-            }
-            option.setAttribute("value", i - 1);
+                option.disabled = "disabled";
+            }  
 
+            if(i == (parseInt(this.item.status) + 1)) {
+                option.selected = "true";
+            }
+
+            option.setAttribute("value", i - 1);
+            option.setAttribute("id", i - 1);
             dropDown.appendChild(option);
         }
 
@@ -55,11 +60,11 @@ class TodoItem {
         return status;
     }
 
-    buildEdit() {
-        let button = document.createElement("button");
-        button.setAttribute("class", "edit-button");
-        button.setAttribute("value", this.item.value);
-        button.innerHTML = "Edit";
+    buildDelete() {
+        let button = document.createElement("img");
+        button.setAttribute("src", "img/delete.png");
+        button.setAttribute("class", "delete-button");
+        button.setAttribute("id", this.value);
 
         return button;
     }
